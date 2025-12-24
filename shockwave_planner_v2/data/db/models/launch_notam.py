03268cdaf7f92330848c1303a57b42c1d2f98f03
Adam Keating <107606965+csprono@users.noreply.github.com>
2025-12-25 11:51:19 +1300

    add composite primary key to LaunchNotam

--- shockwave_planner_v2/data/db/models/launch_notam.py
+++ shockwave_planner_v2/data/db/models/launch_notam.py
@@ -4,6 +4,8 @@ from .launch import Launch
 from .notam import Notam
 
 class LaunchNotam(models.Model):
+    pk = models.CompositePrimaryKey("launch_id", "serial", primary_key=True)
+
     launch_id = models.ForeignKey(
         Launch,
         on_delete=models.CASCADE
