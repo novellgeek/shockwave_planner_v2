9e1a1ad0458abb9a020b2b8d9558ac5373055267
Adam Keating <107606965+csprono@users.noreply.github.com>
2025-12-24 19:32:51 +1300

    add model for launch_notam

---
+++ shockwave_planner_v2/data/db/models/launch_notam.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from .launch import Launch
+from .notam import Notam
+
+class LaunchNotam(models.Model):
+    launch_id = models.ForeignKey(
+        Launch,
+        on_delete=models.CASCADE
+    )
+
+    serial = models.ForeignKey(
+        Notam,
+        on_delete=models.CASCADE
+    )
+
+    models.UniqueConstraint(
+        fields=["launch_id", "serial"], 
+        name="unique_launch_notam_combo", 
+        violation_error_message="A launch can't have the same NOTAM assigned multiple times to it."
+        )
