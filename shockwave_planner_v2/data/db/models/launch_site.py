fc13dd2e716d6ca12c651d417a54f54ee2ded85d
Adam Keating <107606965+csprono@users.noreply.github.com>
2025-12-25 11:50:59 +1300

    fix LaunchSite table

--- shockwave_planner_v2/data/db/models/launch_site.py
+++ shockwave_planner_v2/data/db/models/launch_site.py
@@ -1,14 +1,17 @@
 from django.db import models
 
 class LaunchSite(models.Model):
-    def __init__(self):
-        location = models.TextField(null=False)
-        launch_pad = models.TextField(null=False)
-        longitude = models.FloatField()
-        latitude = models.FloatField()
-        country = models.TextField()
-        site_type = models.TextField(default="LAUNCH")
-        external_id = models.TextField()
-        turnaround_days = models.IntegerField(default=7)
+    name = models.TextField(null=False)
+    launch_pad = models.TextField(null=False)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    country = models.TextField()
+    site_type = models.TextField(default="LAUNCH")
+    external_id = models.TextField()
+    turnaround_days = models.IntegerField(default=7)
 
-        models.UniqueConstraint(fields=["location","launch_pad"])
+    models.UniqueConstraint(
+        fields=["location","launch_pad"], 
+        name="unique_location_launchpad_combo",
+        violation_error_message="A Launch site can't have duplicate launch pads"
+        )
