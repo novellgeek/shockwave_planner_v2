ec2bc68eef69c6f6238ca280912f76bec813f98a
Adam Keating <107606965+csprono@users.noreply.github.com>
2025-12-25 13:23:31 +1300

    fix LaunchStatus and Rocket model definitions

--- shockwave_planner_v2/data/db/models/launch_status.py
+++ shockwave_planner_v2/data/db/models/launch_status.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 class LaunchStatus(models.Model):
-    def __init__(self):
-        name = models.TextField(null=False, unique=True)
-        abbr = models.TextField()
-        colour = models.TextField()
-        description = models.TextField()
+    name = models.TextField(null=False, unique=True)
+    abbr = models.TextField()
+    colour = models.TextField()
+    description = models.TextField()
